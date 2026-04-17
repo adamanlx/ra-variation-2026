dc4602a00dba824939e536141bd189f5332de569
unknown <adamanlinguistics@gmail.com>
2026-04-17 11:10:01 -0500

    clean up initial setup

--- ra-variation.R
+++ ra-variation.R
@@ -4,75 +4,57 @@ library(lmerTest)
 library(gridExtra)
 library(ggpubr)
 
-df <- read.csv(file.choose(), header=T) # select transformed_data.csv
+widen <- function(df, values_from) {
+  return(df %>%
+           pivot_wider(id_cols=c("RESPONDENT_ID", "AGE", "GENDER", "REGION",
+                                 "NORTHWEST", "NORTHWEST_DIALECT"),
+                       names_from="CONDITION_NAME",
+                       values_from=values_from) %>%
+           mutate(HAB0INDngo = (HAB0INDngo1 + HAB0INDngo2) / 2) %>%
+           mutate(HABraINDngo = (HABraINDngo1 + HABraINDngo2) / 2) %>%
+           mutate(PROG0INDDP = (PROG0INDDP1 + PROG0INDDP2) / 2) %>%
+           mutate(PROG0INDngo = (PROG0INDngo1 + PROG0INDngo2) / 2) %>%
+           mutate(PROG0INDko = (PROG0INDko1 + PROG0INDko2) / 2) %>%
+           mutate(PROG0NEG = (PROG0NEG1 + PROG0NEG2) / 2) %>%
+           mutate(PROG0REL = (PROG0REL1 + PROG0REL2) / 2) %>%
+           mutate(PROG0PART = (PROG0PART1 + PROG0PART2) / 2) %>%
+           mutate(PROGraINDDP = (PROGraINDDP1 + PROGraINDDP2) / 2) %>%
+           mutate(PROGraINDfinal = (PROGraINDfinal1 + PROGraINDfinal2) / 2) %>%
+           mutate(PROGraINDngo = (PROGraINDngo1 + PROGraINDngo2) / 2) %>%
+           mutate(PROGraINDko = (PROGraINDko1 + PROGraINDko2) / 2) %>%
+           mutate(PROGraNEG = (PROGraNEG1 + PROGraNEG2) / 2) %>%
+           mutate(PROGraREL = (PROGraREL1 + PROGraREL2) / 2) %>%
+           mutate(PROGraPART = (PROGraPART1 + PROGraPART2) / 2) %>%
+           mutate(FUT0INDDP = (FUT0INDDP1 + FUT0INDDP2) / 2) %>%
+           mutate(FUT0INDngo = (FUT0INDngo1 + FUT0INDngo2) / 2) %>%
+           mutate(FUT0INDko = (FUT0INDko1 + FUT0INDko2) / 2) %>%
+           mutate(FUT0NEG = (FUT0NEG1 + FUT0NEG2) / 2) %>%
+           mutate(FUT0REL = (FUT0REL1 + FUT0REL2) / 2) %>%
+           mutate(FUT0PART = (FUT0PART1 + FUT0PART2) / 2) %>%
+           mutate(FUTraINDDP = (FUTraINDDP1 + FUTraINDDP2) / 2) %>%
+           mutate(FUTraINDfinal = (FUTraINDfinal1 + FUTraINDfinal2) / 2) %>%
+           mutate(FUTraINDngo = (FUTraINDngo1 + FUTraINDngo2) / 2) %>%
+           mutate(FUTraINDko = (FUTraINDko1 + FUTraINDko2) / 2) %>%
+           mutate(FUTraNEG = (FUTraNEG1 + FUTraNEG2) / 2) %>%
+           mutate(FUTraREL = (FUTraREL1 + FUTraREL2) / 2) %>%
+           mutate(FUTraPART = (FUTraPART1 + FUTraPART2) / 2) %>%
+           select(-contains(c("1", "2"))))
+}
 
-# add variables
-df$TAMMORPHEME <- paste(df$TAM, df$MORPHEME, sep="")
-
-df$NORTHWEST <- "Elsewhere"
-df$NORTHWEST[df$REGION%in%c("Burera", "Musanze", "Rulindo", "Gakenke",
-                                    "Rubavu")] <- "Northwest"
-
-df$NORTHWEST_DIALECT <- "Elsewhere"
-df$NORTHWEST_DIALECT[df$IKIGOYI] <- "Northwest"
-df$NORTHWEST_DIALECT[df$IKIRERA] <- "Northwest"
-df$NORTHWEST_DIALECT <- as.factor(df$NORTHWEST_DIALECT)
-
-df$AGERANGE <- "20-29"
-df$AGERANGE[df$AGE >= 30] <- "30-39"
-df$AGERANGE[df$AGE >= 40] <- "40-49"
-df$AGERANGE[df$AGE >= 50] <- "50-59"
-
-df$GENDERREGION <- "Elsewhere female"
-df$GENDERREGION[df$GENDER == "male" & df$NORTHWEST == "Elsewhere"] <- "Elsewhere male"
-df$GENDERREGION[df$GENDER == "female" & df$NORTHWEST == "Northwest"] <- "Northwest female"
-df$GENDERREGION[df$GENDER == "male" & df$NORTHWEST == "Northwest"] <- "Northwest male"
-
-# scale responses
-df = df %>%
+df <- read.csv(file.choose(), header=T) %>% # select transformed_data.csv
+  mutate(TAMMORPHEME = paste(TAM, MORPHEME, sep="")) %>%
+  mutate(NORTHWEST = ifelse(
+    REGION %in% c("Burera", "Musanze", "Rulindo", "Gakenke", "Rubavu"),
+    "Northwest", "Elsewhere"
+  )) %>%
+  mutate(NORTHWEST_DIALECT = ifelse(
+    IKIGOYI | IKIRERA,
+    "Northwest",
+    "Elsewhere"
+  )) %>%
   group_by(RESPONDENT_ID) %>%
   mutate(SCALED_WOULD_YOU_SAY_THIS = scale(WOULD_YOU_SAY_THIS)) %>%
   mutate(SCALED_AWARENESS = scale(HAVE_YOU_HEARD_THIS))
-
-df$MORPHEME = relevel(as_factor(df$MORPHEME), ref="ra")
-
-widen <- function(df, values_from) {
-  return(df %>%
-    pivot_wider(id_cols=c("RESPONDENT_ID", "AGE", "GENDER", "REGION",
-                          "NORTHWEST", "NORTHWEST_DIALECT", "AGERANGE",
-                          "GENDERREGION"),
-                names_from="CONDITION_NAME",
-                values_from=values_from) %>%
-    mutate(HAB0INDngo = (HAB0INDngo1 + HAB0INDngo2) / 2) %>%
-    mutate(HABraINDngo = (HABraINDngo1 + HABraINDngo2) / 2) %>%
-    mutate(PROG0INDDP = (PROG0INDDP1 + PROG0INDDP2) / 2) %>%
-    mutate(PROG0INDngo = (PROG0INDngo1 + PROG0INDngo2) / 2) %>%
-    mutate(PROG0INDko = (PROG0INDko1 + PROG0INDko2) / 2) %>%
-    mutate(PROG0NEG = (PROG0NEG1 + PROG0NEG2) / 2) %>%
-    mutate(PROG0REL = (PROG0REL1 + PROG0REL2) / 2) %>%
-    mutate(PROG0PART = (PROG0PART1 + PROG0PART2) / 2) %>%
-    mutate(PROGraINDDP = (PROGraINDDP1 + PROGraINDDP2) / 2) %>%
-    mutate(PROGraINDfinal = (PROGraINDfinal1 + PROGraINDfinal2) / 2) %>%
-    mutate(PROGraINDngo = (PROGraINDngo1 + PROGraINDngo2) / 2) %>%
-    mutate(PROGraINDko = (PROGraINDko1 + PROGraINDko2) / 2) %>%
-    mutate(PROGraNEG = (PROGraNEG1 + PROGraNEG2) / 2) %>%
-    mutate(PROGraREL = (PROGraREL1 + PROGraREL2) / 2) %>%
-    mutate(PROGraPART = (PROGraPART1 + PROGraPART2) / 2) %>%
-    mutate(FUT0INDDP = (FUT0INDDP1 + FUT0INDDP2) / 2) %>%
-    mutate(FUT0INDngo = (FUT0INDngo1 + FUT0INDngo2) / 2) %>%
-    mutate(FUT0INDko = (FUT0INDko1 + FUT0INDko2) / 2) %>%
-    mutate(FUT0NEG = (FUT0NEG1 + FUT0NEG2) / 2) %>%
-    mutate(FUT0REL = (FUT0REL1 + FUT0REL2) / 2) %>%
-    mutate(FUT0PART = (FUT0PART1 + FUT0PART2) / 2) %>%
-    mutate(FUTraINDDP = (FUTraINDDP1 + FUTraINDDP2) / 2) %>%
-    mutate(FUTraINDfinal = (FUTraINDfinal1 + FUTraINDfinal2) / 2) %>%
-    mutate(FUTraINDngo = (FUTraINDngo1 + FUTraINDngo2) / 2) %>%
-    mutate(FUTraINDko = (FUTraINDko1 + FUTraINDko2) / 2) %>%
-    mutate(FUTraNEG = (FUTraNEG1 + FUTraNEG2) / 2) %>%
-    mutate(FUTraREL = (FUTraREL1 + FUTraREL2) / 2) %>%
-    mutate(FUTraPART = (FUTraPART1 + FUTraPART2) / 2) %>%
-    select(-contains(c("1", "2"))))
-}
 
 # tag respondents based on whether they accept prog or fut
 
@@ -229,6 +211,11 @@ summary(
         MORPHEME %in% c("ra", "0"))
   )
 )
+
+df %>%
+  filter(((TAM == "PROG" & ACCEPTS_PROG == "True") | (TAM == "FUT" & ACCEPTS_FUT == "True") | TAM == "HAB"),
+         MORPHEME == "ra") %>%
+  ggplot(aes(AGE, SCALED_WOULD_YOU_SAY_THIS, color=GENDER, linetype=NORTHWEST)) + geom_jitter(width=0.1, height=0.1) + geom_smooth(method="lm", se=FALSE)
 
 # SECTION 5.3.4 MEANING OF PROG/FUT RA-
 
