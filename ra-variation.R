4f87884e8cc13c7739961f7fb790780a906c5254
unknown <adamanlinguistics@gmail.com>
2026-05-10 17:18:49 -0500

    remove widen

--- ra-variation.R
+++ ra-variation.R
@@ -4,44 +4,7 @@ library(lmerTest)
 library(gridExtra)
 library(ggpubr)
 
-widen <- function(df, values_from) {
-  return(df %>%
-           pivot_wider(id_cols=c("RESPONDENT_ID", "AGE", "GENDER", "REGION",
-                                 "NORTHWEST", "NORTHWEST_DIALECT"),
-                       names_from="CONDITION_NAME",
-                       values_from=values_from) %>%
-           mutate(HAB0INDngo = (HAB0INDngo1 + HAB0INDngo2) / 2) %>%
-           mutate(HABraINDngo = (HABraINDngo1 + HABraINDngo2) / 2) %>%
-           mutate(PROG0INDDP = (PROG0INDDP1 + PROG0INDDP2) / 2) %>%
-           mutate(PROG0INDngo = (PROG0INDngo1 + PROG0INDngo2) / 2) %>%
-           mutate(PROG0INDko = (PROG0INDko1 + PROG0INDko2) / 2) %>%
-           mutate(PROG0NEG = (PROG0NEG1 + PROG0NEG2) / 2) %>%
-           mutate(PROG0REL = (PROG0REL1 + PROG0REL2) / 2) %>%
-           mutate(PROG0PART = (PROG0PART1 + PROG0PART2) / 2) %>%
-           mutate(PROGraINDDP = (PROGraINDDP1 + PROGraINDDP2) / 2) %>%
-           mutate(PROGraINDfinal = (PROGraINDfinal1 + PROGraINDfinal2) / 2) %>%
-           mutate(PROGraINDngo = (PROGraINDngo1 + PROGraINDngo2) / 2) %>%
-           mutate(PROGraINDko = (PROGraINDko1 + PROGraINDko2) / 2) %>%
-           mutate(PROGraNEG = (PROGraNEG1 + PROGraNEG2) / 2) %>%
-           mutate(PROGraREL = (PROGraREL1 + PROGraREL2) / 2) %>%
-           mutate(PROGraPART = (PROGraPART1 + PROGraPART2) / 2) %>%
-           mutate(FUT0INDDP = (FUT0INDDP1 + FUT0INDDP2) / 2) %>%
-           mutate(FUT0INDngo = (FUT0INDngo1 + FUT0INDngo2) / 2) %>%
-           mutate(FUT0INDko = (FUT0INDko1 + FUT0INDko2) / 2) %>%
-           mutate(FUT0NEG = (FUT0NEG1 + FUT0NEG2) / 2) %>%
-           mutate(FUT0REL = (FUT0REL1 + FUT0REL2) / 2) %>%
-           mutate(FUT0PART = (FUT0PART1 + FUT0PART2) / 2) %>%
-           mutate(FUTraINDDP = (FUTraINDDP1 + FUTraINDDP2) / 2) %>%
-           mutate(FUTraINDfinal = (FUTraINDfinal1 + FUTraINDfinal2) / 2) %>%
-           mutate(FUTraINDngo = (FUTraINDngo1 + FUTraINDngo2) / 2) %>%
-           mutate(FUTraINDko = (FUTraINDko1 + FUTraINDko2) / 2) %>%
-           mutate(FUTraNEG = (FUTraNEG1 + FUTraNEG2) / 2) %>%
-           mutate(FUTraREL = (FUTraREL1 + FUTraREL2) / 2) %>%
-           mutate(FUTraPART = (FUTraPART1 + FUTraPART2) / 2) %>%
-           select(-contains(c("1", "2"))))
-}
-
-df <- read.csv(file.choose(), header=T) %>% # select transformed_data.csv
+df <- read.csv('transformed_data.csv', header=T) %>%
   mutate(TAMMORPHEME = paste(TAM, MORPHEME, sep="")) %>%
   mutate(NORTHWEST = ifelse(
     REGION %in% c("Burera", "Musanze", "Rulindo", "Gakenke", "Rubavu"),
@@ -56,19 +19,25 @@ df <- read.csv(file.choose(), header=T) %>% # select transformed_data.csv
   mutate(SCALED_WOULD_YOU_SAY_THIS = scale(WOULD_YOU_SAY_THIS)) %>%
   mutate(SCALED_AWARENESS = scale(HAVE_YOU_HEARD_THIS))
 
+df_avg <- df %>%
+  group_by(RESPONDENT_ID, TAM, MORPHEME, FRAME) %>%
+  summarize(
+    WOULD_YOU_SAY_THIS = mean(WOULD_YOU_SAY_THIS),
+    SCALED_WOULD_YOU_SAY_THIS = mean(SCALED_WOULD_YOU_SAY_THIS)
+  )
+
 # tag respondents based on whether they accept prog or fut
 
-accepts_prog = widen(df, "SCALED_WOULD_YOU_SAY_THIS") %>%
-  filter(PROGraINDfinal > 0) %>%
+accepts_prog = df_avg %>%
+  filter(TAM == "PROG", MORPHEME == "ra", FRAME == "INDfinal",
+         SCALED_WOULD_YOU_SAY_THIS > 0) %>%
   pull(RESPONDENT_ID) %>%
   unique()
-accepts_fut = widen(df, "SCALED_WOULD_YOU_SAY_THIS") %>%
-  filter(FUTraINDfinal > 0) %>%
+accepts_fut = df_avg %>%
+  filter(TAM == "FUT", MORPHEME == "ra", FRAME == "INDfinal",
+         SCALED_WOULD_YOU_SAY_THIS > 0) %>%
   pull(RESPONDENT_ID) %>%
   unique()
-df %>%
-  mutate(ACCEPTS_PROG = RESPONDENT_ID %in% accepts_prog) %>%
-  mutate(ACCEPTS_FUT = RESPONDENT_ID %in% accepts_fut)
 
 # SECTION 5.3.2 OVERALL RESPONSES
 
@@ -117,50 +86,28 @@ accepts_fut %>% unique() %>% length()
 intersect(accepts_prog, accepts_fut) %>% unique() %>% length()
 union(accepts_prog, accepts_fut) %>% unique() %>% length()
 
-merge(
-  merge(
-    widen(df, "WOULD_YOU_SAY_THIS") %>%
-      filter(RESPONDENT_ID %in% accepts_prog) %>%
-      select(starts_with("PROG0") | starts_with("PROGra")) %>%
-      colMeans() %>%
-      enframe(name = "CONDITION", value = "PROG_MEAN"),
-    
-    widen(df, "SCALED_WOULD_YOU_SAY_THIS") %>%
-      filter(RESPONDENT_ID %in% accepts_prog) %>%
-      select(starts_with("PROG0") | starts_with("PROGra"))  %>%
-      colMeans() %>%
-      enframe(name = "CONDITION", value = "PROG_SCALED_MEAN")
-  ) %>%
-    mutate(CONDITION = sub("PROG", "", CONDITION)),
-  
-  merge(
-    widen(df, "WOULD_YOU_SAY_THIS") %>%
-      filter(RESPONDENT_ID %in% accepts_fut) %>%
-      select(starts_with("FUT0") | starts_with("FUTra")) %>%
-      colMeans() %>%
-      enframe(name = "CONDITION", value = "FUT_MEAN"),
-    
-    widen(df, "SCALED_WOULD_YOU_SAY_THIS") %>%
-      filter(RESPONDENT_ID %in% accepts_fut) %>%
-      select(starts_with("FUT0") | starts_with("FUTra")) %>%
-      colMeans() %>%
-      enframe(name = "CONDITION", value = "FUT_SCALED_MEAN")
-  ) %>%
-    mutate(CONDITION = sub("FUT", "", CONDITION)),
-) %>%
-  mutate(across(where(is.numeric), round, digits = 2))
+df_avg %>%
+  filter(RESPONDENT_ID %in% accepts_prog, TAM == "PROG") %>%
+  group_by(MORPHEME, FRAME) %>%
+  summarize(MEAN_WOULD_YOU_SAY_THIS = mean(WOULD_YOU_SAY_THIS),
+            MEAN_SCALED_WOULD_YOU_SAY_THIS = mean(SCALED_WOULD_YOU_SAY_THIS))
 
-
-# SECTION 5.3.4 MEANING OF PROG/FUT RA-
+df_avg %>%
+  filter(RESPONDENT_ID %in% accepts_fut, TAM == "FUT") %>%
+  group_by(MORPHEME, FRAME) %>%
+  summarize(MEAN_WOULD_YOU_SAY_THIS = mean(WOULD_YOU_SAY_THIS),
+            MEAN_SCALED_WOULD_YOU_SAY_THIS = mean(SCALED_WOULD_YOU_SAY_THIS))
 
 grid.arrange(
-  widen(df, "WOULD_YOU_SAY_THIS") %>%
-    select(PROGraINDfinal, FUTraINDfinal) %>%
-    ggplot(aes(PROGraINDfinal, FUTraINDfinal))+geom_jitter(width=0.1, height=0.1)+
+  df_avg %>%
+    filter(MORPHEME == "ra", FRAME == "INDfinal", TAM %in% c("PROG", "FUT")) %>%
+    pivot_wider(id_cols = RESPONDENT_ID, names_from = TAM, values_from = WOULD_YOU_SAY_THIS) %>%
+    ggplot(aes(PROG, FUT))+geom_jitter(width=0.1, height=0.1)+
     labs(title="unscaled", x="PROG", y="FUT")+theme(plot.title = element_text(hjust = 0.5)),
-  widen(df, "SCALED_WOULD_YOU_SAY_THIS") %>%
-    select(PROGraINDfinal, FUTraINDfinal) %>%
-    ggplot(aes(PROGraINDfinal, FUTraINDfinal))+geom_jitter(width=0.1, height=0.1)+
+  df_avg %>%
+    filter(MORPHEME == "ra", FRAME == "INDfinal", TAM %in% c("PROG", "FUT")) %>%
+    pivot_wider(id_cols = RESPONDENT_ID, names_from = TAM, values_from = SCALED_WOULD_YOU_SAY_THIS) %>%
+    ggplot(aes(PROG, FUT))+geom_jitter(width=0.1, height=0.1)+
     labs(title="scaled", x="PROG", y="FUT")+theme(plot.title = element_text(hjust = 0.5))+
     geom_vline(xintercept=0)+geom_hline(yintercept=0),
   ncol=2
@@ -168,27 +115,10 @@ grid.arrange(
 
 # are PROG and FUT comparable?
 
-prog_fut_responses = widen(df, "SCALED_WOULD_YOU_SAY_THIS") %>%
-  filter(RESPONDENT_ID %in% accepts_prog, RESPONDENT_ID %in% accepts_fut) %>%
-  select(starts_with("FUT") | starts_with("PROGra") | starts_with("PROG0")) %>%
-  pivot_longer(names_to = "CONDITION", values_to = "SCORE", cols = !c("RESPONDENT_ID")) %>%
-  mutate(TAMMORPHEME = case_when(
-    startsWith(CONDITION, "FUT0") ~ "FUT0",
-    startsWith(CONDITION, "FUTra") ~ "FUTra",
-    startsWith(CONDITION, "PROG0") ~ "PROG0",
-    startsWith(CONDITION, "PROGra") ~ "PROGra"
-  )) %>%
-  mutate(FRAME = case_when(
-    endsWith(CONDITION, "INDfinal") ~ "INDfinal",
-    endsWith(CONDITION, "INDDP") ~ "INDDP",
-    endsWith(CONDITION, "INDngo") ~ "INDngo",
-    endsWith(CONDITION, "INDko") ~ "INDko",
-    endsWith(CONDITION, "NEG") ~ "NEG",
-    endsWith(CONDITION, "REL") ~ "REL",
-    endsWith(CONDITION, "PART") ~ "PART"
-  )) %>%
-  select(-CONDITION) %>%
-  pivot_wider(names_from = TAMMORPHEME, values_from=SCORE)
+prog_fut_resopnses <- df_avg %>%
+  mutate(TAMMORPHEME = paste(TAM, MORPHEME, sep='')) %>%
+  pivot_wider(id_cols=c(RESPONDENT_ID, FRAME), names_from=TAMMORPHEME, values_from=SCALED_WOULD_YOU_SAY_THIS)
+
 
 grid.arrange(
   prog_fut_responses %>%
@@ -209,18 +139,14 @@ summary(lm(PROG0 ~ FUT0, prog_fut_responses))
 
 # SECTION 5.3.5 ACCEPTABILITY OF RA-LESS VERBS BEFORE NGO
 
-widen(df, "SCALED_WOULD_YOU_SAY_THIS") %>%
-  filter(HABraINDngo > 0, HAB0INDngo > 0) %>%
-  pull(RESPONDENT_ID) %>% length()
-widen(df, "SCALED_WOULD_YOU_SAY_THIS") %>%
-  filter(HABraINDngo <= 0, HAB0INDngo > 0) %>%
-  pull(RESPONDENT_ID) %>% length()
-widen(df, "SCALED_WOULD_YOU_SAY_THIS") %>%
-  filter(HABraINDngo > 0, HAB0INDngo <= 0) %>%
-  pull(RESPONDENT_ID) %>% length()
-widen(df, "SCALED_WOULD_YOU_SAY_THIS") %>%
-  filter(HABraINDngo <= 0, HAB0INDngo <= 0) %>%
-  pull(RESPONDENT_ID) %>% length()
+ngo_responses <- df_avg %>%
+  filter(TAM == "HAB", FRAME == "INDngo") %>%
+  pivot_wider(id_cols=RESPONDENT_ID, names_from=MORPHEME, values_from=SCALED_WOULD_YOU_SAY_THIS)
+
+ngo_responses %>% filter(ra > 0, `0` > 0) %>% nrow()
+ngo_responses %>% filter(ra <= 0, `0` > 0) %>% nrow()
+ngo_responses %>% filter(ra > 0, `0` <= 0) %>% nrow()
+ngo_responses %>% filter(ra <= 0, `0` <= 0) %>% nrow()
 
 summary(lm(WOULD_YOU_SAY_THIS ~ AGE * GENDER * NORTHWEST_DIALECT * MORPHEME,
          data=df %>%
@@ -269,23 +195,10 @@ df %>%
 
 # SECTION 5.3.7 IMPLICATIONAL HIERARCHIES?
 
-neg_rel_part = rbind(
-  widen(df, "SCALED_WOULD_YOU_SAY_THIS") %>%
-    filter(RESPONDENT_ID %in% accepts_prog) %>%
-    select(PROGraNEG, PROGraREL, PROGraPART) %>%
-    rename(NEG = PROGraNEG) %>%
-    rename(REL = PROGraREL) %>%
-    rename(PART = PROGraPART) %>%
-    mutate(TAM = "PROG"),
-  
-  widen(df, "SCALED_WOULD_YOU_SAY_THIS") %>%
-    filter(RESPONDENT_ID %in% accepts_fut) %>%
-    select(FUTraNEG, FUTraREL, FUTraPART) %>%
-    rename(NEG = FUTraNEG) %>%
-    rename(REL = FUTraREL) %>%
-    rename(PART = FUTraPART) %>%
-    mutate(TAM = "FUT")
-)
+neg_rel_part <- df_avg %>%
+  filter(TAM %in% c("PROG", "FUT"), FRAME %in% c("NEG", "REL", "PART"), MORPHEME == "ra",
+         (TAM == "PROG" & RESPONDENT_ID %in% accepts_prog) | (TAM == "FUT" & RESPONDENT_ID %in% accepts_fut)) %>%
+  pivot_wider(id_cols=c(RESPONDENT_ID, TAM), names_from=FRAME, values_from=SCALED_WOULD_YOU_SAY_THIS)
 
 ggarrange(
   neg_rel_part %>%
@@ -305,7 +218,7 @@ ggarrange(
   
   neg_rel_part %>%
     ggplot(
-      aes(REL, PART, color=TAM)
+      aes(PART, REL, color=TAM)
     ) + geom_jitter() +
     xlab("relativization") + ylab("participial")+
     xlim(-1.5, 1.5)+ylim(-1.5, 1.5),
@@ -327,28 +240,6 @@ summary(
 summary(
   lmer(PART ~ REL * TAM + (1 | RESPONDENT_ID),
        data = neg_rel_part))
-
-neg_rel_part %>%
-  filter(TAM == "PROG", NEG < 0, REL < 0, PART < 0) %>%
-  nrow()
-
-for (tam in c("PROG", "FUT")) {
-  for (neg in c(TRUE, FALSE)) {
-    for (rel in c(TRUE, FALSE)) {
-      for (part in c(TRUE, FALSE)) {
-        print(c(tam, neg, rel, part,
-                neg_rel_part %>%
-                  filter(
-                    TAM == tam,
-                    ifelse(neg, NEG >= 0, NEG < 0),
-                    ifelse(rel, REL >= 0, REL < 0),
-                    ifelse(part, PART >= 0, PART < 0),
-                    ) %>%
-                  nrow()))
-      }
-    }
-  }
-}
 
 # SECTION 5.3.8 PERIPHRASTICS
 
